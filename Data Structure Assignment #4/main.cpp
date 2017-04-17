ebd965515ae5e15e6cab7a6a4fe2f18627a79d27
mikezander <mikezander87@yahoo.com>
2017-04-17 13:44:26 -0400

    Initial Commit

---
+++ Data Structure Assignment #4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structure Assignment #4
+//
+//  Created by Michael Alexander on 4/17/17.
+//  Copyright © 2017 Michael Alexander. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
